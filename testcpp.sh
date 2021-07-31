ff5e26657f7b833c37fba475a04e1dbf1d2fc85c
candidate <candidate@programmr.com>
2021-07-31 08:16:43 +0000

    submit

--- testcpp.sh
+++ testcpp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 a=$(pwd)
-command='java Challenge'
+command='g++ Challenge.cpp && ./a.out'
 myVar=`echo $a | sed 's/ *$//g'`
 cd /home/theia 
 echo "Executing the test.."
 
-javac $a/Challenge.java
+
 :> $myVar/.testcaseop2 &&  java -classpath './json-simple-1.1.1.jar:./' Test  $myVar "$command"> $myVar/.testcaseop2
 
 if grep -q 'error: ' $myVar/.testcaseop2; then
